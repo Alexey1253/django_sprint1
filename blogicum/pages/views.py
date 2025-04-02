d9790cce82043d4236f3c1661cecc8f62470c0e5
Alexey Lazarev <alexeylazarev53@gmail.com>
2025-04-02 10:34:43 +1000

    blogicum

---
+++ blogicum/pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+from django.http import HttpRequest
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    template_name = "pages/about.html"
+
+    return render(request, template_name, {})
+
+
+def rules(request: HttpRequest) -> HttpResponse:
+    template_name = "pages/rules.html"
+
+    return render(request, template_name, {})
